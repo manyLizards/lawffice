9a166c045b87f25c4b7082bb90dd00790ef79f08
Toliver Crisp <toliverjcrisp@gmail.com>
2022-10-24 20:01:51 -0400

    changed the variables to vanilla js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-var reviewsDiv = $("#reviews")
-var scheduleDiv = $("#schedule")
-var contactDiv = $("#contact")
-var videosDiv = $("#videos")
-var servicesDiv = $("#services")
+// DOM variables
+var reviewsDiv = document.querySelector("#reviews")
+var scheduleDiv = document.querySelector("#schedule")
+var contactDiv = document.querySelector("#contact")
+var videosDiv = document.querySelector("#videos")
+var servicesDiv = document.querySelector("#services")
+
+
+
+
+
+
