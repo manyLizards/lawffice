eccec9b822241d44f387623f7b53c172ba4337a2
iskry <refocuslucifer@protonmail.com>
2022-10-24 20:51:04 -0500

    added comments

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
+// element variables
 var reviewsDiv = document.getElementById("reviews")
 var scheduleDiv = document.getElementById("schedule")
 var contactDiv = document.getElementById("contact")
 var videosDiv = document.getElementById("videos")
 var servicesDiv = document.getElementById("services")
 
+// geo location API key
+let locationAPIKey = 'f0500e4ecfa1403e916ccc44a848a8d5';
 
-let apiKey = 'f0500e4ecfa1403e916ccc44a848a8d5';
-
-fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey)
+// fetch request for user info
+fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=' + locationAPIKey)
   .then((response) => response.json())
   .then((data) => locationVerify(data));
 
+// conditional to verify if location is in area of operation. data is passed through by fetch request
 function locationVerify(data) {
     if(data.region_iso_code != "TX") {
         contactDiv.append("Maisie does not service your area")
